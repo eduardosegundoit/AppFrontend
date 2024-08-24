825c8e61d36fe6038bc54f26451e0bc430bc3982
Carlos Eduardo Segundo Dos Santos <eduardolondon93@gmail.com>
2024-08-24 13:52:15 +0100

    teste3

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,18 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo', '@babel/preset-react', '@babel/preset-env'],
+    presets: [
+      'babel-preset-expo',
+      '@babel/preset-env', // Ensure this is first to handle env-specific transforms
+      '@babel/preset-react', // Preset for React
+    ],
     plugins: [
       '@babel/plugin-transform-export-namespace-from',
       ['@babel/plugin-transform-class-properties', {loose: true}],
       ['@babel/plugin-transform-private-methods', {loose: true}],
       ['@babel/plugin-transform-private-property-in-object', {loose: true}],
-      ['@babel/plugin-transform-react-jsx', {runtime: 'automatic'}],
-      'react-native-reanimated/plugin',
+      ['@babel/plugin-transform-react-jsx', {runtime: 'automatic'}], // Ensure this is after env and react presets
+      'react-native-reanimated/plugin', // Always include Reanimated's plugin
     ],
   };
 };
--- metro.config.js
+++ metro.config.js
@@ -1,37 +1,38 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
-const path = require('path');
+/* eslint-disable prettier/prettier */
+const { getDefaultConfig } = require('@expo/metro-config');
 
+// Get default configurations from Expo's Metro config
 const defaultConfig = getDefaultConfig(__dirname);
 
-defaultConfig.resolver.resolveRequest = (context, moduleName, platform) => {
-  if (platform === 'web') {
-    const replacements = {
-      'react-native/Libraries/Utilities/Platform':
-        'react-native-web/dist/exports/Platform',
-      'react-native/Libraries/Components/AccessibilityInfo/legacySendAccessibilityEvent':
-        'react-native-web/dist/exports/AccessibilityInfo',
-      'react-native/Libraries/StyleSheet/PlatformColorValueTypes':
-        'react-native-web/dist/exports/StyleSheet',
-      'react-native/Libraries/NativeComponent/PlatformBaseViewConfig':
-        'identity-obj-proxy',
-      'react-native/Libraries/Network/RCTNetworking': 'identity-obj-proxy',
-      'react-native/Libraries/Alert/RCTAlertManager': 'identity-obj-proxy',
-      'react-native/Libraries/Image/Image': 'identity-obj-proxy',
-      'react-native/Libraries/Utilities/BackHandler':
-        'react-native-web/dist/exports/BackHandler',
-    };
-
-    const replacement = replacements[moduleName];
-
-    if (replacement) {
-      return {
-        filePath: require.resolve(replacement),
-        type: 'sourceFile',
+// Add any customizations to the defaultConfig here
+// Ensure compatibility with React Native Web and other modules
+defaultConfig.resolver = {
+  ...defaultConfig.resolver,
+  resolveRequest: (context, moduleName, platform) => {
+    if (platform === 'web') {
+      const replacements = {
+        'react-native/Libraries/Utilities/Platform': 'react-native-web/dist/exports/Platform',
+        'react-native/Libraries/Components/AccessibilityInfo/legacySendAccessibilityEvent': 'react-native-web/dist/exports/AccessibilityInfo',
+        'react-native/Libraries/StyleSheet/PlatformColorValueTypes': 'react-native-web/dist/exports/StyleSheet',
+        'react-native/Libraries/NativeComponent/PlatformBaseViewConfig': 'identity-obj-proxy',
+        'react-native/Libraries/Network/RCTNetworking': 'identity-obj-proxy',
+        'react-native/Libraries/Alert/RCTAlertManager': 'identity-obj-proxy',
+        'react-native/Libraries/Image/Image': 'identity-obj-proxy',
+        'react-native/Libraries/Utilities/BackHandler': 'react-native-web/dist/exports/BackHandler',
       };
-    }
-  }
 
-  return context.resolveRequest(context, moduleName, platform);
+      const replacement = replacements[moduleName];
+
+      if (replacement) {
+        return {
+          filePath: require.resolve(replacement),
+          type: 'sourceFile',
+        };
+      }
+    }
+
+    return context.resolveRequest(context, moduleName, platform);
+  }
 };
 
-module.exports = mergeConfig(defaultConfig, {});
+module.exports = defaultConfig;
